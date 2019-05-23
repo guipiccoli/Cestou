a702477e7b257d3e74b5ad8d8975f54ccf679f5b
Rafael Ferreira <rafaelfca.ferreira@gmail.com>
2019-05-23 15:20:02 -0300

    Flipped the bar chart sideways

--- Cestou/Controllers/Charts/MonthlyPlanningTableViewCell.swift
+++ Cestou/Controllers/Charts/MonthlyPlanningTableViewCell.swift
@@ -11,12 +11,8 @@ import Charts
 
 class MonthlyPlanningTableViewCell: UITableViewCell {
     
-    @IBOutlet var balancoLabel: UILabel!
-    @IBOutlet var gastoProjetadoLabel: UILabel!
-    @IBOutlet var rendaLabel: UILabel!
-    @IBOutlet var gastosLabel: UILabel!
     
-    @IBOutlet weak var monthlyPlanningChart: BarChartView!
+    @IBOutlet weak var monthlyPlanningChart: HorizontalBarChartView!
     @IBOutlet weak var backgroundCardView: UIView!
     @IBOutlet var noDataView: UIView!
     @IBOutlet var noDataText: UILabel!
@@ -50,33 +46,25 @@ class MonthlyPlanningTableViewCell: UITableViewCell {
         backgroundCardView.layer.borderColor = UIColor.lightGray.cgColor
         backgroundCardView.layer.masksToBounds = false
         
-        
-        
-        balancoLabel.text = String(format: "R$%.2f", (self.getBalance["Incoming"]! - self.getBalance["Expenses"]!))
-        gastoProjetadoLabel.text = String(format: "R$%.2f", (self.getBalance["Planning"]!))
-        rendaLabel.text = String(format: "R$%.2f", (self.getBalance["Incoming"]!))
-        gastosLabel.text = String(format: "R$%.2f", (self.getBalance["Expenses"]!))
 
     }
     
     func setChart() {
         
         //valores de entrada pro grafico
-        let entry1 = BarChartDataEntry(x: 1.0, y: getBalance["Incoming"]!)
-        let entry2 = BarChartDataEntry(x: 2.0, y: getBalance["Planning"]!)
-        let entry3 = BarChartDataEntry(x: 3.0, y: getBalance["Expenses"]!)
+        let entry1 = BarChartDataEntry(x: 2.0, y: getBalance["Planning"]!)
+        let entry2 = BarChartDataEntry(x: 3.0, y: getBalance["Expenses"]!)
 
         //dataset
-        let dataSet = BarChartDataSet(entries: [entry1,entry2,entry3], label: "Gastos Totais")
+        let dataSet = BarChartDataSet(entries: [entry2,entry1], label: "Gastos Totais")
         let data = BarChartData(dataSets: [dataSet])
         monthlyPlanningChart.data = data
 
-        let greenColor = NSUIColor.init(red: 0.0/255, green: 136.0/255, blue: 15.0/255, alpha: 1.0)
         let blueColor = NSUIColor.init(red: 0.0/255, green: 146.0/255, blue: 186.0/255, alpha: 1.0)
         let redColor = NSUIColor.init(red: 189.0/255, green: 0.0/255, blue: 0.0/255, alpha: 1.0)
 
         
-        dataSet.setColors([greenColor,blueColor,redColor], alpha: 1.0)
+        dataSet.setColors([blueColor,redColor], alpha: 1.0)
         
     
         monthlyPlanningChart.leftAxis.enabled = false
@@ -84,6 +72,8 @@ class MonthlyPlanningTableViewCell: UITableViewCell {
         monthlyPlanningChart.xAxis.enabled = false
         monthlyPlanningChart.leftAxis.axisMinimum = 0
         monthlyPlanningChart.highlightPerTapEnabled = false
+        monthlyPlanningChart.layer.cornerRadius = 10
+        
         
         monthlyPlanningChart.legend.enabled = false
         data.setDrawValues(false)
